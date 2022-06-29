47ef9357ebfa167e9d2e9c78083b031264012c2c
John Garbero <john.garbero.itb@gmail.com>
2022-06-29 11:15:01 -0500

    commit tree 1bd5a152f34db7f2564261445d8153ec0bc3a32d

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # your code goes here
+
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness = (0..10), :hygiene = (0..10)
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name=name
+        @bank_account=bank_account
+        @happiness=happiness
+        @hygiene=hygiene
+    end
+end
