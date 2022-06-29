153ea7ed2cec1aab90997030c32a87c6df5b02f6
John Garbero <john.garbero.itb@gmail.com>
2022-06-29 11:15:54 -0500

    passed some tests

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # your code goes here
+
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name=name
+        @bank_account=bank_account
+        @happiness=happiness
+        @hygiene=hygiene
+    end
+end
